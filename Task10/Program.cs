0463e4512a3628c7cf068fe714b2ffb5c1421d8e
Olga Semenova <olya.meoow@yandex.ru>
2022-10-30 18:48:51 +0300

    task10 not done yet

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int FirstLastNumber (int number);
+{
+    int FirstLastNumber = num / 10;
+}
+
+Console.WriteLine(FirstLastNumber(num)); 
