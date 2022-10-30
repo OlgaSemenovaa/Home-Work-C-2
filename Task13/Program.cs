1f1cd714263a44b2f0b522cdbe4480c44b3e42b3
Olga Semenova <olya.meoow@yandex.ru>
2022-10-31 00:37:07 +0300

    changes in folders

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,7 +6,22 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.Write("Введи число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+// if (anyNumberText.Length > 2)
+// {
+//   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+// }
+// else {
+//   Console.WriteLine("-> третьей цифры нет");
+// }
+
 int num = new Random().Next();
 Console.WriteLine(num);
 
-
+while (num)
+{
+    num / 10;
+    num % 10;
+}
